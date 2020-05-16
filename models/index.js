f47d1b980f3d71a7f83f9cbcb719d741d87deea5
Michael Jeffcoat <38878555+mikegjeff@users.noreply.github.com>
2020-05-17 11:35:49 +1200

    Initial Commit

---
+++ models/index.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGODB_URI);
+
+// When successfully connected
+mongoose.connection.on('connected', () => {
+    console.log('Established Mongoose Default Connection');
+});
+
+// When connection throws an error
+mongoose.connection.on('error', err => {
+    console.log('Mongoose Default Connection Error : ' + err);
+});
