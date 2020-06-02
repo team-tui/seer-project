0a3fa055ff204d3b0e44a6d382ad6f9c507cf5b0
Sam Ward <s.w.ward@hotmail.com>
2020-06-02 21:47:56 +1200

    Expanded Article Schema and mocha/chai testing

--- models/Books.js
+++ models/Books.js
@@ -1,17 +1,34 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const statusSchema = require('./Status')
 
-const bookSchema = new Schema({
-	title: {
+const articleSchema = new Schema({
+ 	title: {
 		type: String,
 		required: true
 	},
 	author: {
 		type: String,
 		required: true
+	}, 
+ 	results: {
+		type: String,
+		required: true
+	}, 
+	date: {
+		type: Date,
+		required: true
+	}, 
+	url: {
+		type: String,
+		required: true
+	},  
+ 	status: {
+		type: statusSchema,
+		required: true
 	}
 });
 
-const Book = mongoose.model('Book', bookSchema);
+const Article = mongoose.model('SEER', articleSchema, 'articles');
 
-module.exports = Book;
+module.exports = Article;
